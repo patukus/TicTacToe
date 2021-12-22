20b3601f5f90d1cca142d44be04f3d59e3a7fbac
Patryk Radecki <patukus@gmail.com>
2021-12-23 00:13:38 +0100

    Narysowana tablica, zdeklarowana tabela i zczytywanie kliknięć

--- main.py
+++ main.py
@@ -2,21 +2,70 @@ import sys
 
 import pygame
 
+import numpy as np
+
 pygame.init()
 
 WIDTH = 600
 HEIGHT = 600
+LINE_WIDTH = 15
+BOARD_ROWS = 3
+BOARD_COLS = 3
 
 RED = (255, 0, 0)
+BG_COLOR = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TIC TAC TOE')
-screen.fill(RED)
+screen.fill(BG_COLOR)
+
+# board
+board = np.zeros((BOARD_ROWS, BOARD_COLS))
+
+
+def draw_lines():
+    # horizontal 1
+    pygame.draw.line(screen, LINE_COLOR, (0, 200), (600, 200), LINE_WIDTH)
+    # horizontal 2
+    pygame.draw.line(screen, LINE_COLOR, (0, 400), (600, 400), LINE_WIDTH)
+    # vertical 1
+    pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
+    # vertical 2
+    pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+
+
+def mark_square(row, col, player):
+    board[row][col] = player
+
+
+def available_square(row, col):
+    return board[row][col] == 0
+
+
+def is_board_full():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if available_square(row, col):
+                return False
+
+    return True
+
+
+draw_lines()
 
 # mainloop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            mouseX = event.pos[0]
+            mouseY = event.pos[1]
+
+            clicked_row = int(mouseY // 200)
+            clicked_col = int(mouseX // 200)
 
     pygame.display.update()
