bcd760f567bcf07656d23d8ea925493ddc01d473
Patryk Radecki <patukus@gmail.com>
2021-12-23 00:37:08 +0100

    Rysowanie figur wstępnie

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ HEIGHT = 600
 LINE_WIDTH = 15
 BOARD_ROWS = 3
 BOARD_COLS = 3
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
+CROSS_WIDTH = 25
 
 RED = (255, 0, 0)
 BG_COLOR = (28, 170, 156)
 LINE_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TIC TAC TOE')
@@ -35,8 +39,18 @@ def draw_lines():
     pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
 
 
-def mark_square(row, col, player):
-    board[row][col] = player
+def draw_figuers():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 1:
+                pygame.draw.circle(screen, CIRCLE_COLOR, (int(col * 200 + 100), int(row * 200 + 100)), CIRCLE_RADIUS,
+                                   CIRCLE_WIDTH)
+            elif board[row][col] == 2:
+                pygame.draw.line(screen, RED, (col * 200, row * 200 + 200), (col * 200 + 200, row * 200), CROSS_WIDTH)
+
+
+def mark_square(row, col, num):
+    board[row][col] = num
 
 
 def available_square(row, col):
@@ -54,6 +68,8 @@ def is_board_full():
 
 draw_lines()
 
+player = 1
+
 # mainloop
 while True:
     for event in pygame.event.get():
@@ -67,5 +83,15 @@ while True:
 
             clicked_row = int(mouseY // 200)
             clicked_col = int(mouseX // 200)
+
+            if available_square(clicked_row, clicked_col):
+                if player == 1:
+                    mark_square(clicked_row, clicked_col, player)
+                    player = 2
+                else:
+                    mark_square(clicked_row, clicked_col, player)
+                    player = 1
+
+                draw_figuers()
 
     pygame.display.update()
